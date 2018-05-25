643672db0d4fd2eac70bffd949f30aee24373cc6
Iabin Arteaga <iabin@ciencias.unam.mx>
2018-05-25 12:58:46 -0500

    agregando verificacion

--- src/main/java/controlador/Registrar.java
+++ src/main/java/controlador/Registrar.java
@@ -1,12 +1,20 @@
 package controlador;
 
-import java.util.Locale;
+import java.util.Properties;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import modelo.Usuario;
 import modelo.UsuarioDAO;
+
 
 
 /**
@@ -19,6 +27,9 @@ public class Registrar {
     private String nombre;
     private String correo;
     private String contrasena;
+    
+      private static final String us= "dragonsoftis";
+    private static final String pass = "dragonsoft";
     
     
     public String getNombre() {
@@ -45,7 +56,40 @@ public class Registrar {
         this.contrasena = contrasena;
     }
     
-   
+   public void enviarCorreo(String email, String asunto, String mensaje){
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+ 
+        Session session;
+        session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    @Override
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(us, pass);
+                    }
+                });
+ 
+        try {
+ 
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(us));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(email));
+            message.setSubject(asunto);
+            message.setText(mensaje);
+ 
+            Transport.send(message);
+ 
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+        
+    }
+
  
     /**
      * Metodo que hace persistente a un usario en la base de datos
@@ -67,12 +111,22 @@ public class Registrar {
            
             
             UsuarioDAO usuarioDAO = new UsuarioDAO();
+            for(Usuario o:usuarioDAO.usuarios()){
+                if (o.getCorreo().equalsIgnoreCase(correo)){
+                     FacesContext context = FacesContext.getCurrentInstance();
+                    context.addMessage(null, new FacesMessage("Ese correo ya fue registrado"));
+                    return "";
+                }
+            }
+          
+                
             Boolean r = usuarioDAO.nuevoUsuario(usuario);
             if(!r){
                 FacesContext context = FacesContext.getCurrentInstance();
                 context.addMessage(null, new FacesMessage("Agregado incorrectamente"));
                 return "ingresar.xhtml?faces-redirect=true";
             }
+            enviarCorreo(correo, "confirma tu registro", "intenta iniciar sesion para comprobar el registro");
         
     return "index.xhtml?faces-redirect=true"; 
     }
